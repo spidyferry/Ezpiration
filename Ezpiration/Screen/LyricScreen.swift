7abc759bd521914b30583d98390bd0de6c01506a
spidyferry <1988ferry@gmail.com>
2021-06-24 23:36:32 +0700

    Navigation OK, record STT OK, load stt from core data OK Todo : predicate result STT

--- Ezpiration/Screen/LyricScreen.swift
+++ Ezpiration/Screen/LyricScreen.swift
@@ -10,12 +10,35 @@ import CoreData
 
 class LyricScreen: UIViewController {
     
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var records:[Records]?
+    
+    @IBOutlet weak var lyricText: UILabel!
+    @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var recordTtl: UILabel!
     var lyricTitle = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(lyricTitle)
+        
+        fetchRecords()
+        saveButton.isEnabled = false
         recordTtl?.text = lyricTitle
+        
+        lyricText.text = records?[0].stt_result
     }
+    
+    func fetchRecords(){
+        do {
+            self.records = try context.fetch(Records.fetchRequest())
+            DispatchQueue.main.async {
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+//    @IBAction func lyricDidEdited(_ sender: UITextField) {
+//        saveButton.isEnabled = sender.text!.count > 0
+//    }
 }
